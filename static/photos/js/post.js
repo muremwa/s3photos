55b622080f395dbbe1ae254ec03e27124475b2a4
Muremwa <danmburu254@gmail.com>
2020-09-27 12:50:22 +0300

    not sure why I ever ignored these ¯\_(ツ)_/¯

---
+++ static/photos/js/post.js
@@ -0,0 +1,23 @@
+$(document).on('click', '.liking', function () {
+    var img = this;
+    var url = this.dataset["likeUrl"];
+    var token = this.parentElement.children['csrfmiddlewaretoken'].value;
+    var value = this.parentElement.children['count'];
+    
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: {
+            csrfmiddlewaretoken: token,
+        },
+        success: function (response) {
+            if (response['state'] == 'liked') {
+                img.src = '/static/svg/heart_full.svg';
+            } else {
+                img.src = '/static/svg/heart.svg';
+            }
+            value.innerText=response['likes'];            
+        }
+    });
+
+});
